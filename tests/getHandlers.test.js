be75da38fce30245b15b95bdc06bb91ea12fe5aa
kedlam2 <kedlam2@gmail.com>
2024-09-07 12:41:09 -0400

    Update getHandlers.test.js

--- tests/getHandlers.test.js
+++ tests/getHandlers.test.js
@@ -1,10 +1,12 @@
 // eslint-disable-next-line no-undef
 const config = require('../config');
 
-test('', async () => {
+test('Should return 200 status code', async () => {
 	try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`);
+		const response = await fetch(`${config.API_URL}/api/v1/kits/1`);
+		actualStatuscode = response.status;
 	} catch (error) {
 		console.error(error);
 	}
+	expect(actualStatuscode).toBE(200)
 });
