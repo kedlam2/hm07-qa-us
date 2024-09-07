ab329aac350d4fad0bf53209d91116210de6619b
kedlam2 <kedlam2@gmail.com>
2024-09-07 12:45:14 -0400

    Update getHandlers.test.js

--- tests/getHandlers.test.js
+++ tests/getHandlers.test.js
@@ -2,6 +2,7 @@
 const config = require('../config');
 
 test('Should return 200 status code', async () => {
+	let actualStatuscode;
 	try {
 		const response = await fetch(`${config.API_URL}/api/v1/kits/1`);
 		actualStatuscode = response.status;
@@ -9,4 +10,15 @@ test('Should return 200 status code', async () => {
 		console.error(error);
 	}
 	expect(actualStatuscode).toBE(200)
+});
+
+test('Body should contain ...', async () => {
+	let actualResponseBody;
+	try {
+		const response = await fetch(`${config.API_URL}/api/v1/kits/1`);
+		actualResponseBody = await response.json();
+	} catch (error) {
+		console.error(error);
+	}
+	expect(actualResponseBody.name).toBE("For picnic")
 });
